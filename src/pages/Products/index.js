152d4d9f28fad8b274a6113ed98c6da76c03edb7
sotopaguayj <sotopaguayj@gmail.com>
2023-03-10 12:01:32 -0500

    bugs fixed

--- src/pages/Products/index.js
+++ src/pages/Products/index.js
@@ -131,7 +131,11 @@ function Index() {
       </div>
       {load 
       ? <div className='h-screen flex justify-center items-center'>
-          <img src='loader.svg' />
+          <div class="flex items-center justify-center space-x-2">
+            <div className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] text-teal-500 motion-reduce:animate-[spin_1.5s_linear_infinite]" role="status">
+              <span class="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">Loading...</span>
+            </div>
+          </div>
         </div> 
       :
       value.length != 0 
